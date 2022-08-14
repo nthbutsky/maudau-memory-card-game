301da4f814343b3b74aad5d9e4d8750bc313f895
Denis Butsky <butskyi@convidera.com>
2022-08-14 23:46:03 +0300

    fix

--- dist/assets/login.57ae6f12.js
+++
@@ -1,1 +0,0 @@
-import{d as t,a as s,g as a,o as i,c,f as e}from"./index.b1225f40.js";const r=e("h1",null,"Login",-1),d=e("div",{id:"firebaseui-auth-container"},null,-1),_=e("div",{id:"loader"}," Loading... ",-1),u=[r,d,_],m=t({__name:"login",setup(l){const{ui:o,uiConfig:n}=s();return a(()=>{o.start("#firebaseui-auth-container",n)}),(f,h)=>(i(),c("div",null,u))}});export{m as default};

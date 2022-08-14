0d8418477bfe210b0a9f1de8f230385ef830c16b
Denis Butsky <butskyi@convidera.com>
2022-08-14 22:21:07 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,8 @@
 set -e
 # build
 npm run build
-# navigate into the build output directory
-cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:nthbutsky/maudau-memory-card-game.git chief:gh-pages
-cd -
+git subtree push --prefix dist origin gh-pages

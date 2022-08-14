eb0713a796d9fecb4d7913091f3f0ba826cf8542
Denis Butsky <butskyi@convidera.com>
2022-08-15 00:02:41 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:nthbutsky/nthbutsky.github.io.git chief
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nthbutsky/maudau-memory-card-game.git chief:gh-pages
+# git push -f git@github.com:nthbutsky/maudau-memory-card-game.git chief:gh-pages
 
 cd -
